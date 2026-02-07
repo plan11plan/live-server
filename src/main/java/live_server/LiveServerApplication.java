4839dc95a9a6c28606aacc04e04e0308136aab00
plan11plan <tjtfy7028@gmail.com>
2026-02-07 21:32:41 +0900

    init

---
+++ src/main/java/live_server/LiveServerApplication.java
@@ -0,0 +1,13 @@
+package live_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/live_server/LiveServerApplicationTests.java
@@ -0,0 +1,13 @@
+package live_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
